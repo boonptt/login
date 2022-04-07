4e393745f706962fde08558618a33a68f99c1d21
boonptt <thanhpt.ba9057@st.usth.edu.vn>
2022-04-07 10:17:38 +0700

    form

---
+++ main.js
@@ -0,0 +1,11 @@
+var fullName = "Nguyen Van A";
+var age = 20;
+var job = "Student";
+
+alert(`${fullName} is ${age} years old and works as a ${job}`);
+console.log(`${fullName} is ${age} years old and works as a ${job}`);
+
+console.error("Error message");
+console.warn("Warning message");
+
+confirm("Are you sure?");
